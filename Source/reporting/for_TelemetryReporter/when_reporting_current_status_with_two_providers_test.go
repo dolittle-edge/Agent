6b45346e2c61d7f8b55c96a22d1d8ce2ea18f0dd
Einar Ingebrigtsen <einar@dolittle.com>
2019-05-20 12:10:06 +0200

    Moving things around

---
+++ Source/reporting/for_TelemetryReporter/when_reporting_current_status_with_two_providers_test.go
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package main_test
+
+import "testing"
+import "github.com/dolittle-edge/agent/reporting"
+
+type FakeProvider struct {
+}
+
+func (provider FakeProvider) Provide() []*TelemetrySample {
+	samples := []*TelemetrySample{}
+
+	return samples
+}
+
+func when_reporting_current_status_with_two_providers(T *testing.T) {
+	fp := new(FakeProvider)
+	fp.Provide()
+}
---
+++ Source/reporting/go.mod
@@ -0,0 +1,5 @@
+module github.com/dolittle-edge/agent/reporting
+
+go 1.12
+
+require github.com/cloudfoundry/gosigar v1.1.0
