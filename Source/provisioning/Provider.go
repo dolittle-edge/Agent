dd071c2b657e67e995bc147f29c483b81cfbb489
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-09-04 21:50:10 +0200

    Update configuration automatically when changed in Studio

--- Source/provisioning/Provider.go
+++ Source/provisioning/Provider.go
@@ -8,10 +8,14 @@ import (
 	"agent/log"
 	"agent/provisioning/system"
 	"bytes"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 )
 
@@ -65,7 +69,19 @@ func (p *Provider) runProvider() {
 		if !p.Current.IsValid() {
 			node, wait, err := getNodeConfiguration()
 			if err == nil {
+				log.Informationln("Recieved new node configuration")
+				persistConfiguration(node)
 				p.Current = node
+				p.notifyListeners(node)
+			}
+			time.Sleep(wait)
+		} else {
+			node, changed, wait, err := checkForNodeConfigurationUpdates(p.Current)
+			if err == nil && changed {
+				log.Informationln("Recieved update for node configuration")
+				persistConfiguration(node)
+				p.Current = node
+				p.notifyListeners(node)
 			}
 			time.Sleep(wait)
 		}
@@ -80,6 +96,32 @@ func decodeConfiguration(data []byte) (node Node, err error) {
 	}
 	node.isValid = true
 	return
+}
+
+func encodeConfiguration(node Node) (data []byte, err error) {
+	data, err = json.Marshal(node)
+	if err != nil {
+		log.Errorln("Could not encode Node configuration:", err)
+	}
+	return
+}
+
+func persistConfiguration(node Node) error {
+	if node.IsValid() {
+		data, err := encodeConfiguration(node)
+		err = ioutil.WriteFile("node.json", data, 0700)
+		if err != nil {
+			log.Errorln("Could not write node.json file:", err)
+			return err
+		}
+	} else {
+		err := os.Remove("node.json")
+		if err != nil {
+			log.Errorln("Could not delete node.json file:", err)
+			return err
+		}
+	}
+	return nil
 }
 
 func readPersistedConfiguration() (Node, error) {
@@ -113,7 +155,6 @@ func getNodeConfiguration() (Node, time.Duration, error) {
 	}
 	informationReader := bytes.NewReader(informationJSON)
 
-	// TODO: Close buffers
 	response, err := http.Post(provisioningEndpoint+"/Get", "application/json", informationReader)
 	if err != nil {
 		log.Errorln("Error while getting configuration for node:", err)
@@ -133,12 +174,56 @@ func getNodeConfiguration() (Node, time.Duration, error) {
 		node, err := readRecievedConfiguration(response)
 		if err != nil {
 			log.Errorln("Recieved node configuration not valid")
-			return Node{}, waitWhileNotConfigured, err
+			return Node{}, waitWhileInError, err
 		}
 		return node, waitWhileConfigured, nil
 
 	default:
 		log.Errorln("Unexpected response code from provisioning endpoint:", response.StatusCode)
-		return Node{}, waitWhileNotConfigured, errors.New("Unexpected status code")
+		return Node{}, waitWhileInError, errors.New("Unexpected status code")
+	}
+}
+
+func checkForNodeConfigurationUpdates(current Node) (Node, bool, time.Duration, error) {
+	data, err := encodeConfiguration(current)
+	if err != nil {
+		return Node{}, false, waitWhileInError, err
+	}
+
+	hash := sha256.Sum256(data)
+	information := url.Values{}
+	information.Add("NodeId", current.NodeId)
+	information.Add("Hash", base64.StdEncoding.EncodeToString(hash[0:32]))
+
+	response, err := http.PostForm(provisioningEndpoint+"/Check", information)
+	if err != nil {
+		log.Errorln("Error while checking configuration updates for node:", err)
+		return Node{}, false, waitWhileInError, nil
+	}
+
+	switch response.StatusCode {
+	case http.StatusNotFound:
+		log.Warningln("Node was configured - but could not find it while checking for updates")
+		return Node{}, true, waitWhileNotConfigured, nil
+
+	case http.StatusUnauthorized:
+		log.Informationln("Node configuration was revoked")
+		return Node{}, true, waitWhileNotAuthorized, nil
+
+	case http.StatusNotModified:
+		log.Informationln("Configuration for node not changed")
+		return Node{}, false, waitWhileConfigured, nil
+
+	case http.StatusOK:
+		node, err := readRecievedConfiguration(response)
+		if err != nil {
+			log.Errorln("Recieved node configuration update not valid")
+			return Node{}, false, waitWhileInError, err
+		}
+		return node, true, waitWhileConfigured, nil
+
+	default:
+		log.Errorln("Unexpected response code from provisioning endpoint:", response.StatusCode)
+		return Node{}, false, waitWhileConfigured, errors.New("Unexpected status code")
 	}
 }
